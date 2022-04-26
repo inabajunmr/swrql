c7c0b8153895970d4270013fc41c1b008552d0ac
inabajunmr <inabajun.for.regi@gmail.com>
2022-04-26 16:23:44 +0900

    refactor

--- swrql/src/predicate/predicate.ts
+++ swrql/src/predicate/predicate.ts
@@ -1,66 +1,62 @@
 import { Record } from '../scan/scan';
 import {
-  AndToken,
-  EqualToken,
-  IdentifierToken,
-  NumberToken,
-  OrToken,
-  StringToken,
-  Token,
+    AndToken,
+    EqualToken,
+    IdentifierToken,
+    NumberToken,
+    OrToken,
+    StringToken,
+    Token,
 } from '../sql/token';
 
 export class Predicate {
-  // RPN order tokens
-  readonly tokens: Token[];
-  constructor(tokens: Token[]) {
-    this.tokens = tokens;
-  }
-
-  test(record: Record): boolean {
-    const tempTokens = [...this.tokens];
-    const stack = [];
-    while (tempTokens.length > 0) {
-      const current = tempTokens.shift();
-
-      if (current === EqualToken.TOKEN) {
-        const l = stack.pop();
-        const r = stack.pop();
-        if (l === r) {
-          stack.push('TRUE');
-        } else {
-          stack.push('FALSE');
-        }
-      } else if (current === AndToken.TOKEN) {
-        const l = stack.pop();
-        const r = stack.pop();
-        if (l === 'TRUE' && r === 'TRUE') {
-          stack.push('TRUE');
-        } else {
-          stack.push('FALSE');
-        }
-      } else if (current === OrToken.TOKEN) {
-        const l = stack.pop();
-        const r = stack.pop();
-        if (l === 'TRUE' || r === 'TRUE') {
-          stack.push('TRUE');
-        } else {
-          stack.push('FALSE');
-        }
-      }
-
-      if (current instanceof IdentifierToken) {
-        stack.push(record.get(current.literal));
-      } else if (current instanceof StringToken) {
-        stack.push(current.literal);
-      } else if (current instanceof NumberToken) {
-        stack.push(current.literal);
-      }
+    // RPN order tokens
+    readonly tokens: Token[];
+    constructor(tokens: Token[]) {
+        this.tokens = tokens;
     }
 
-    if (stack.pop() === 'TRUE') {
-      return true;
-    } else {
-      return false;
+    test(record: Record): boolean {
+        const tempTokens = [...this.tokens];
+        const stack: any[] = [];
+        while (tempTokens.length > 0) {
+            const current = tempTokens.shift();
+
+            if (current === EqualToken.TOKEN) {
+                const l = stack.pop();
+                const r = stack.pop();
+                if (l === r) {
+                    stack.push(true);
+                } else {
+                    stack.push(false);
+                }
+            } else if (current === AndToken.TOKEN) {
+                const l = stack.pop();
+                const r = stack.pop();
+                if (l && r) {
+                    stack.push(true);
+                } else {
+                    stack.push(false);
+                }
+            } else if (current === OrToken.TOKEN) {
+                const l = stack.pop();
+                const r = stack.pop();
+                if (l || r) {
+                    stack.push(true);
+                } else {
+                    stack.push(false);
+                }
+            }
+
+            if (current instanceof IdentifierToken) {
+                stack.push(record.get(current.literal));
+            } else if (current instanceof StringToken) {
+                stack.push(current.literal);
+            } else if (current instanceof NumberToken) {
+                stack.push(current.literal);
+            }
+        }
+
+        return stack.pop();
     }
-  }
 }
