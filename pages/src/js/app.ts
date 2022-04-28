2197e5bebe9a4e9efd4a53a187873557ad219c48
inabajunmr <inabajun.for.regi@gmail.com>
2022-04-28 22:39:21 +0900

    multiple tables

--- pages/src/js/app.ts
+++ pages/src/js/app.ts
@@ -1,39 +1,44 @@
 import { stringify } from 'csv-stringify/sync';
 import * as swrql from 'swrql';
 
-export function execute() : void {
+let tableCount = 0;
+addTable();
+(document.getElementById("csv1") as HTMLTextAreaElement).value = `a,b,c
+1,2,3
+x,y,z
+one,two,three`;
+(document.getElementById("sql") as HTMLTextAreaElement).value = `select * from abc where a=1;`;
+(document.getElementById("tablename1") as HTMLTextAreaElement).value = `abc`;
+
+export function execute(): void {
     document.getElementById("error")!.textContent = '';
 
-    const tableName = (document.getElementById("tablename") as any).value;
-    const csv = (document.getElementById("csv") as any).value;
     const sql = (document.getElementById("sql") as any).value;
 
     try {
-        const scan = new swrql.CSVScan(tableName, csv);
-        const result = new swrql.SQLExecution([scan], sql).execute();
-        if(result.records.length === 0) {
+        const scans: swrql.CSVScan[] = [];
+        for (let i = 1; i <= tableCount; i++) {
+            const csv = (document.getElementById("csv" + i) as any).value;
+            const tableName = (document.getElementById("tablename" + i) as any).value;
+            const scan = new swrql.CSVScan(tableName, csv);
+            scans.push(scan);
+        }
+        const result = new swrql.SQLExecution(scans, sql).execute();
+        if (result.records.length === 0) {
             document.getElementById("result")!.textContent = result.fields.join(',');
-        }else {
+        } else {
             document.getElementById("result")!.textContent = stringify(result.records.map(r => r.unwrap()), { header: true });
-        }        
-    } catch(e:unknown) {
-        if(e instanceof Error) {
+        }
+    } catch (e: unknown) {
+        if (e instanceof Error) {
             document.getElementById("error")!.textContent = e.message;
         } else {
             document.getElementById("error")!.textContent = 'Unexpected error.';
         }
     }
 }
-(document.getElementById("csv")as HTMLTextAreaElement).value = `a,b,c
-1,2,3
-x,y,z
-one,two,three`;
 
-(document.getElementById("sql")as HTMLTextAreaElement).value = `select * from abc where a=1;`;
-(document.getElementById("tablename")as HTMLTextAreaElement).value = `abc`;
-
-
-document.getElementById("csv")!.addEventListener("input", () => {
+document.getElementById("csv1")!.addEventListener("input", () => {
     execute();
 })
 
@@ -41,8 +46,50 @@ document.getElementById("sql")!.addEventListener("input", () => {
     execute();
 })
 
-document.getElementById("tablename")!.addEventListener("input", () => {
+document.getElementById("tablename1")!.addEventListener("input", () => {
     execute();
 })
+
+document.getElementById('addtable')!.addEventListener('click', () => {
+    addTable();
+})
+
+function addTable() {
+    tableCount++;
+    const div = document.createElement('div');
+    const span = document.createElement('span');
+    const tableName = document.createElement('input');
+    tableName.id = 'tablename' + tableCount;
+    tableName.type = 'text';
+    tableName.value = 'table' + tableCount;
+    tableName.addEventListener("input", () => {
+        execute();
+    });
+
+    const deleteButton = document.createElement('input');
+    deleteButton.onclick = () => {
+        div.remove();
+        tableCount--;
+        execute();
+    };
+    deleteButton.value = 'remove';
+    deleteButton.type = 'button';
+
+    const csv = document.createElement('textarea');
+    csv.id = 'csv' + tableCount;
+    csv.style.height = '13em';
+    csv.addEventListener("input", () => {
+        execute();
+    });
+
+    span.appendChild(tableName);
+    if (tableCount !== 1) {
+        span.appendChild(deleteButton);
+    }
+    div.appendChild(span);
+    div.appendChild(csv);
+    document.getElementById('csv')?.appendChild(div);
+
+}
 
 execute();
